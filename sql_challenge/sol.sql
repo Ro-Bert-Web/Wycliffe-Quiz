ac07367897ed7e2476cc1ada83fb92d74fd9dd10
Ro-Bert-Web <robertbhs424@gmail.com>
2022-06-15 19:46:03 -0700

    Finished sql challenge

--- sql_challenge/sol.sql
+++ sql_challenge/sol.sql
@@ -1,3 +1,5 @@
 SELECT Title FROM Book where Author = (SELECT id FROM Author WHERE Name = "C.S. Lewis");
 SELECT Title FROM Book WHERE Publication_Year < 1951;
 SELECT Title FROM Book WHERE Author = (SELECT id FROM Author WHERE Name = "C.S. Lewis") ORDER BY Publication_Year;
+SELECT Title FROM Book INNER JOIN Author ON Book.Author = Author.id INNER JOIN Publisher ON Book.Publisher = Publisher.id WHERE Author.City = Publisher.City;
+SELECT Book.Title, Author.Name, min(Publication_Year) FROM Book INNER JOIN Author ON Book.Author = Author.id INNER JOIN Publisher ON Book.Publisher = Publisher.id GROUP BY Author;
